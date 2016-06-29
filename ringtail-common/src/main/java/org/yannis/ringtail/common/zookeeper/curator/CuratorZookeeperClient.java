858cef9d1caa045218d66c797d45f2f15c4c90e8
yannis.zhao <yanjun.zhao@lianshang.com>
2016-06-29 17:00:24 +0800

    placeholder

---
+++ ringtail-common/src/main/java/org/yannis/ringtail/common/zookeeper/curator/CuratorZookeeperClient.java
@@ -0,0 +1,7 @@
+package org.yannis.ringtail.common.zookeeper.curator;
+
+/**
+ * Created by yannis on 6/29/16.
+ */
+public class CuratorZookeeperClient {
+}
---
+++ ringtail-common/src/main/java/org/yannis/ringtail/common/zookeeper/zkclient/ZkZookeeperClient.java
@@ -0,0 +1,7 @@
+package org.yannis.ringtail.common.zookeeper.zkclient;
+
+/**
+ * Created by yannis on 6/29/16.
+ */
+public class ZkZookeeperClient {
+}
